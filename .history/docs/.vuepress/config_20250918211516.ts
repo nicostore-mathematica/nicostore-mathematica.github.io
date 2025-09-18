10c241f40d59c95a50ff691b072de64ca89a914f
nicostore-mathematica <nicostore.core@outlook.com>
2025-09-18 21:16:26 +0800

    twikoo

--- .history/docs/.vuepress/config_20250918211516.ts
+++ .history/docs/.vuepress/config_20250918211516.ts
@@ -107,24 +107,24 @@ export default defineUserConfig({
       * markdown
       * @see https://theme-plume.vuejs.press/config/markdown/
       */
-     markdown: {
+    // markdown: {
     //   abbr: true,         // 启用 abbr 语法  *[label]: content
     //   annotation: true,   // 启用 annotation 语法  [+label]: content
-       pdf: true,          // 启用 PDF 嵌入 @[pdf](/xxx.pdf)
+    //   pdf: true,          // 启用 PDF 嵌入 @[pdf](/xxx.pdf)
     //   caniuse: true,      // 启用 caniuse 语法  @[caniuse](feature_name)
-       plot: true,         // 启用隐秘文本语法 !!xxxx!!
+    //   plot: true,         // 启用隐秘文本语法 !!xxxx!!
     //   bilibili: true,     // 启用嵌入 bilibili视频 语法 @[bilibili](bid)
     //   youtube: true,      // 启用嵌入 youtube视频 语法 @[youtube](video_id)
     //   artPlayer: true,    // 启用嵌入 artPlayer 本地视频 语法 @[artPlayer](url)
     //   audioReader: true,  // 启用嵌入音频朗读功能 语法 @[audioReader](url)
-       icon: { provider: 'iconify' },        // 启用内置图标语法  ::icon-name::
+    //   icon: { provider: 'iconify' },        // 启用内置图标语法  ::icon-name::
     //   table: true,        // 启用表格增强容器语法 ::: table
     //   codepen: true,      // 启用嵌入 codepen 语法 @[codepen](user/slash)
     //   replit: true,       // 启用嵌入 replit 语法 @[replit](user/repl-name)
     //   codeSandbox: true,  // 启用嵌入 codeSandbox 语法 @[codeSandbox](id)
     //   jsfiddle: true,     // 启用嵌入 jsfiddle 语法 @[jsfiddle](user/id)
     //   npmTo: true,        // 启用 npm-to 容器  ::: npm-to
-       demo: true,         // 启用 demo 容器  ::: demo
+    //   demo: true,         // 启用 demo 容器  ::: demo
     //   repl: {             // 启用 代码演示容器
     //     go: true,         // ::: go-repl
     //     rust: true,       // ::: rust-repl
@@ -145,8 +145,8 @@ export default defineUserConfig({
     //     size: true,       // 启用图片大小
     //   },
     //   include: true,      // 在 Markdown 文件中导入其他 markdown 文件内容
-       imageSize: 'local', // 启用 自动填充 图片宽高属性，避免页面抖动
-     },
+    //   imageSize: 'local', // 启用 自动填充 图片宽高属性，避免页面抖动
+    // },
 
     /**
      * 水印
